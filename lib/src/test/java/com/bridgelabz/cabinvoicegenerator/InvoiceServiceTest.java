5954c8f6b5c7e8d9a24ee7327e147fe0b277fc84
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-22 20:18:35 +0530

    ADDED: test case for calculating fare

--- lib/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ lib/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -2,10 +2,16 @@ package com.bridgelabz.cabinvoicegenerator;
 
 import org.junit.Test;
 
-import junit.framework.Assert;
 
 public class InvoiceServiceTest {
 	
-
+	@Test
+	public void givenDistanceAndTime_shouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		assertEquals(25, fare);
+	}
 
 }
