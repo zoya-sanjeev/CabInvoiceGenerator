b14adac7d744c31b3576d5a327e6e449d2ecae8d
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-22 21:05:06 +0530

    MODIFIED: calulcateFare for including minimum fare condition

--- lib/src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ lib/src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -4,10 +4,14 @@ public class InvoiceGenerator {
 
 	private static final double MINIMUN_COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUN_FARE = 5;
 
 	public double calculateFare(double distance, int time) {
 		
-		return distance * MINIMUN_COST_PER_KILOMETER + time * COST_PER_TIME;
+		double totalFare= distance * MINIMUN_COST_PER_KILOMETER + time * COST_PER_TIME;
+		if(totalFare< MINIMUN_FARE)
+			return MINIMUN_FARE;
+		return totalFare;
 	}
 
 
