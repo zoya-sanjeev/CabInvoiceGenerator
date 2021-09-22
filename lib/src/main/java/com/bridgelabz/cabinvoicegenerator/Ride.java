64905bdea6d6f8beac28eebf3214c709b6882f21
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-22 23:14:05 +0530

    ADDED: enum RideType

--- lib/src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
+++ lib/src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -4,6 +4,11 @@ public class Ride {
 
 	public double distance;
 	public int time;
+	
+	enum RideType{
+		NORMAL_RIDE,
+		PREMIUM_RIDE
+	}
 
 	public Ride(double distance, int time) {
 		this.distance=distance;
