f22904f7783ef80e556f43b05af7c5f0c4ba80eb
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-22 21:48:29 +0530

    REFACTORED: used Before for initializing InvoiceGenerator object

--- lib/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ lib/src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -4,14 +4,20 @@ package com.bridgelabz.cabinvoicegenerator;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class InvoiceServiceTest {
+	InvoiceGenerator invoiceGenerator=null;
+	
+	@Before
+	public void setUp() throws Exception{
+		invoiceGenerator= new InvoiceGenerator();
+	}
 	
 	@Test
 	public void givenDistanceAndTime_shouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 2.0;
 		int time = 5;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -20,7 +26,6 @@ public class InvoiceServiceTest {
 	
 	@Test
 	public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
@@ -29,7 +34,6 @@ public class InvoiceServiceTest {
 	
 	@Test
 	public void givenMultipleWrites_ShouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
 		double fare = invoiceGenerator.calculateFare(rides);
 		assertEquals(30, fare, 0.0);
