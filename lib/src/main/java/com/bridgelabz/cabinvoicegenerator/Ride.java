98004c8b73eeb552590d5b541ec423d95399c870
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-22 21:16:16 +0530

    ADDED: Ride class added

--- lib/src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
+++ lib/src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -2,4 +2,13 @@ package com.bridgelabz.cabinvoicegenerator;
 
 public class Ride {
 
+	private double distance;
+	private int time;
+
+	public Ride(double distance, int time) {
+		this.distance=distance;
+		this.time=time;
+	}
+
+
 }
